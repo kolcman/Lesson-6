0a488796d36feba1658eac6f37ee901dbcc104f3
Kolcman <m.kolcman@yandex.ru>
2024-02-19 13:58:01 +0300

    Lesson6:Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,18 @@
 ﻿
+System.Console.WriteLine("Введите пароль: ");
+string str = Console.ReadLine();
+
+System.Console.WriteLine(GetLetters(str));
+
+string GetLetters(string str)
+{
+    string finalString = "";
+    foreach (char i in str)
+    {
+        if(char.IsAsciiLetter(i) == true)
+        {
+            finalString = finalString + i;
+        }
+    }
+    return finalString;
+}
