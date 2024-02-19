8ed72c2b1953de8fd1a62c97dcc7465a04e6d80e
Kolcman <m.kolcman@yandex.ru>
2024-02-19 12:18:12 +0300

    First commit

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Task 1
+
+int[,] table = new int[3, 5];
+
+Random rnd = new Random();
+for (int i = 0; i < table.GetLength(0); i++)
+{
+    for (int j = 0; j < table.GetLength(1); j++)
+    {
+        table[i, j] = rnd.Next(1, 11);
+    }
+}
+
+for (int i = 0; i < table.GetLength(0); i++)
+{
+    for (int j = 0; j < table.GetLength(1); j++)
+    {
+        System.Console.Write($"{table[i, j]} ");
+    }
+    Console.WriteLine();
+}
